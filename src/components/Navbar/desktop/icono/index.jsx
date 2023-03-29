b6fee983e40868a33350b257dfe2fb1680885b23
EmaCaceress <EmanuelChacon1@outlook.com>
2023-03-29 00:42:22 -0300

    modificacion de navbar

--- src/components/Navbar/desktop/icono/index.jsx
+++ src/components/Navbar/desktop/icono/index.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const Icon = () => {
   return (
     <>
-      <div className="navbarDesktop__logo__icon">
-        <div className="navbarDesktop__logo__icon__linea" />
-
-        <div className="navbarDesktop__logo__icon__cartel">
+      <div className="navbarDesktop__icon">
+        <div className="navbarDesktop__icon__cartel">
           <h1>OPEN</h1>
         </div>
       </div>
