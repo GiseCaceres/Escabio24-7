e4b195fdc5b7a2e984c2434c69dcaf950c3550b9
EmaCaceress <EmanuelChacon1@outlook.com>
2023-05-18 14:59:06 -0300

    Agregando interacciones basicas del aside con el main

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,29 +3,53 @@ import { useParams } from "react-router-dom";
 import { data } from "../../utils/data";
 import Cards from "./ContentCard/index";
 
-const Card = () => {
+const Card = ({ optional, search, priceLess, priceBigger }) => {
   const [items, setItems] = useState([]);
   const { category } = useParams();
 
+  function removeDiacritic(texto) {
+    return texto
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/\p{Diacritic}/gu, "");
+  }
   useEffect(() => {
-    setItems([]);
-    if (category !== undefined)
-      setItems(data.filter((item) => item.category == category));
-    else setItems(data);
-  }, [category]);
+    console.log(priceLess, priceBigger);
+    let filtrado = category
+      ? data.filter((item) => item.category == category)
+      : (optional && data.filter((item) => item.category == optional)) || data;
+
+    if (search) {
+      filtrado = filtrado.filter((item) =>
+        removeDiacritic(item.title).includes(removeDiacritic(search))
+      );
+    }
+    if (priceLess || priceBigger) {
+      filtrado = filtrado.filter(
+        (item) =>
+          item.price >= (priceLess || 0) &&
+          item.price <= (priceBigger || Infinity)
+      );
+    }
+    setItems(filtrado);
+  }, [category, optional, search, priceLess, priceBigger]);
 
   return (
     <>
-      {items.map((obj, id) => (
-        <Cards
-          key={id}
-          img={obj.img}
-          title={obj.title}
-          description={obj.description}
-          price={obj.price}
-          id={obj.id}
-        ></Cards>
-      ))}
+      {items.length > 0 ? (
+        items.map((obj, id) => (
+          <Cards
+            key={id}
+            img={obj.img}
+            title={obj.title}
+            description={obj.description}
+            price={obj.price}
+            id={obj.id}
+          ></Cards>
+        ))
+      ) : (
+        <h1>No hay productos de esta indole</h1>
+      )}
     </>
   );
 };
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Card from "../Card";
 
 const Products = () => {
+  const [option, setOption] = useState(undefined);
+  const [search, setSearch] = useState("");
+  const [priceLess, setPriceLess] = useState(null);
+  const [priceBigger, setPriceBigger] = useState(null);
   return (
     <>
       <main className="products">
@@ -8,48 +13,77 @@ const Products = () => {
           <h1 className="products__aside__title">Productos</h1>
           <section className="products__aside__search">
             <h1>Buscar</h1>
-            <input className="" type="text" placeholder="Buscar..." />
+            <input
+              className=""
+              type="text"
+              placeholder="Buscar..."
+              onChange={(data) => {
+                setSearch(data.target.value);
+              }}
+              value={search}
+            />
           </section>
           <ul className="products__aside__list">
             <li className="active">
               <a>Categorias</a>
             </li>
-            <li>
+            <li onClick={() => setOption(undefined)}>
+              <a>todo</a>
+            </li>
+            <li onClick={() => setOption(1)}>
               <a>Fernet</a>
             </li>
-            <li>
+            <li onClick={() => setOption(2)}>
               <a>Vodka</a>
             </li>
-            <li>
+            <li onClick={() => setOption(7)}>
               <a>Espumantes</a>
             </li>
-            <li>
-              <a>Vino tinto</a>
+            <li onClick={() => setOption(5)}>
+              <a>Ron</a>
             </li>
-            <li>
+            <li onClick={() => setOption(8)}>
               <a>Cerveza</a>
             </li>
-            <li>
+            <li onClick={() => setOption(3)}>
               <a>Gin tonic</a>
             </li>
-            <li>
-              <a>Gancia</a>
+            <li onClick={() => setOption(6)}>
+              <a>Aperitivos</a>
             </li>
-            <li>
+            <li onClick={() => setOption(4)}>
               <a>Wisky</a>
             </li>
-            <li>
-              <a>otros</a>
+            <li onClick={() => setOption(9)}>
+              <a>Comestible</a>
             </li>
           </ul>
           <section className="products__aside__range">
             <h1>rango de precios</h1>
-            <input className="" type="text" placeholder="$1000" /> -{" "}
-            <input className="" type="text" placeholder="$5000" />
+            <input
+              className=""
+              type="number"
+              placeholder="$1000"
+              onChange={(data) => setPriceLess(data.target.value)}
+              value={priceLess}
+            />{" "}
+            -{" "}
+            <input
+              className=""
+              type="number"
+              placeholder="$5000"
+              onChange={(data) => setPriceBigger(data.target.value)}
+              value={priceBigger}
+            />
           </section>
         </aside>
         <main className="products__main">
-          <Card></Card>
+          <Card
+            optional={option}
+            search={search}
+            priceLess={priceLess}
+            priceBigger={priceBigger}
+          ></Card>
         </main>
       </main>
     </>
